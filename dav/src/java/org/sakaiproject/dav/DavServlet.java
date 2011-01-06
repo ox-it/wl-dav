ea197cbaabceaf2484797db4d990e0d6a6fb3e91
Matthew Buckett <matthew.buckett@oucs.ox.ac.uk>
2011-01-06 12:34:55 +0000

    WL-1112 Give correct error when deleting folder fails. When you don't have permission to delete a folder we not give an error rather than saying it worked.

--- dav/src/java/org/sakaiproject/dav/DavServlet.java
+++ dav/src/java/org/sakaiproject/dav/DavServlet.java
@@ -4183,6 +4183,7 @@ public class DavServlet extends HttpServlet
 		}
 		catch (PermissionException e)
 		{
+			resp.sendError(SakaidavStatus.SC_FORBIDDEN);
 			return false;
 		}
 		catch (InUseException e)
